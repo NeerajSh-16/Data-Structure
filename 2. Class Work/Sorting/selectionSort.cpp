daf0db9fb85c2bc14642b38f90162870a361195c
Neeraj.Sh-16 <shneeraj.2619@gmail.com>
2024-03-08 09:53:27 +0530

    Selection Sort Added

--- 2. Class Work/Sorting/selectionSort.cpp
+++ 2. Class Work/Sorting/selectionSort.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 using namespace std;
+int findSmallest(int *arr,int size, int index){
+    int result = 0;
+    for(int i=index;i<size;i++){
+        for(int j=index;j<size;j++){
+            if(arr[j] < arr[i])
+                result = j;
+        }
+    }
+    return result;
+}
+void swap(int *arr, int a, int b){
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+}
+void selectionSort(int *arr, int size){
+    int smallestElementIndex = 0;
+    int result = 0;
+    for(int i=1;i<size;i++){
+        result = findSmallest(arr,size, smallestElementIndex);
+        swap(arr, smallestElementIndex, result);
+        smallestElementIndex++;
+    }
+    cout<<"Array after sorting : ";
+    for(int i=0;i<size;i++){
+        cout<<arr[i]<<" ";
+    }
+}
 int main(){
     int size;
     cout<<"Enter the size of yoyr array : ";
@@ -9,5 +37,6 @@ int main(){
     for(int i=0;i<size;i++){
         cin>>arr[i];
     }
+    selectionSort(arr, size);
     return 0;
 }
